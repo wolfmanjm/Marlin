04c85f36731dc404f3038336930c2511b15c2707
Nicolas Rossi <nixouille@gmail.com>
2013-03-05 14:52:51 +0100

    Toggle menu for PSU from LCD pannel
    From the prepare menu, accessible when is not printing, you have the
    possibility to turn off the PSU when is on et vice versa.
    From the host, you can turn off or turn on the PSU then the menu is
    updated accordingly.
    From the LCD message, the printer status is reported ready or off
    respectively when the PSU is on or off.

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -38,6 +38,7 @@ void copy_and_scalePID_d();
 /* Different menus */
 static void lcd_status_screen();
 #ifdef ULTIPANEL
+extern bool powersupply;
 static void lcd_main_menu();
 static void lcd_tune_menu();
 static void lcd_prepare_menu();
@@ -303,6 +304,12 @@ static void lcd_prepare_menu()
     MENU_ITEM(function, MSG_PREHEAT_PLA, lcd_preheat_pla);
     MENU_ITEM(function, MSG_PREHEAT_ABS, lcd_preheat_abs);
     MENU_ITEM(gcode, MSG_COOLDOWN, PSTR("M104 S0\nM140 S0"));
+    if (powersupply)
+    {
+        MENU_ITEM(gcode, MSG_SWITCH_PS_OFF, PSTR("M81"));
+    }else{
+        MENU_ITEM(gcode, MSG_SWITCH_PS_ON, PSTR("M80"));
+    }
     MENU_ITEM(submenu, MSG_MOVE_AXIS, lcd_move_menu);
     END_MENU();
 }
